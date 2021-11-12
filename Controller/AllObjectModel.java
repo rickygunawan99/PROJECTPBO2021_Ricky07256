573b92a2871a250ac6cfb9f24b75dd0079812e5b
rickygunawan99 <rickygunawan266@gmail.com>
2021-11-12 21:54:46 +0700

    del

--- Controller/AllObjectModel.java
+++
@@ -1,9 +0,0 @@
-package Controller;
-
-import Model.AdminModel;
-import Model.SepatuModel;
-
-public class AllObjectModel {
-    public static AdminModel adminModel = new AdminModel();
-    public static SepatuModel sepatuModel = new SepatuModel();
-}
--- process07256/Interface07256.java
+++
@@ -1,5 +0,0 @@
-package process07256;
-
-public interface Interface07256 {
-    public void viewData();
-}
